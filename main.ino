9dec4996766eb234f8156ba94ae4d3ee3d0c18bd
bl44ck <bl4ckcr4nb3rry@gmail.com>
2022-03-12 22:52:06 +0100

    init

--- main.ino
+++ main.ino
@@ -1,12 +1,31 @@
-// First we include the libraries
-//#include <Wire.h> activate for Arduino
+//Pin connected to SH_CP of 74HC595(SR) - pin 11
+int ClockPin = 6;
+//Pin connected to ST_CP of SR pin 12
+int LatchPin = 5;
+//Pin connected to DS of SR pin 14
+int DataPin = 2;
+//Pin connected to -OE of SR pin 13
+int OePin = 3;
+//Pin connected to -MR of SR pin 10
+int MrPin = 4;
+//Pin connected to LED
+int ledRPin = 10;
+int ledGPin = 9;
+int ledBPin = 8;
+//Pin connected to Switch 1
+int switchOne = 15;
+//Pin connected to Switch 2
+int switchTwo = 14;
+//Pin connected to Buzzer
+int buzzerPin = 7;
+//Pin to for US-Sensory
+int UsTrgPin = 11;
+int UsEchPin = 12;
 
-//Pin connected to SH_CP of 74HC595 - pin 11 on ic
-int ClockPin = 2;
-//Pin connected to ST_CP of 74HC595 - pin 12 on ic
-int LatchPin = 3;
-//Pin connected to DS of 74HC595 - pin 14 on ic
-int DataPin = 4;
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 rtc;
+Adafruit_BME280 bme;
 
 int PlusPin = A1;
 int MinusPin = A2;
@@ -18,157 +37,211 @@ int MinusPin = A2;
 //pointer
 byte * registerPattern;
 
-int test = 0;
-
 
 void setup() {
-  
+
     //Serial Output
     Serial.begin(9600);
 
-    //set pins to output so you can control the shift register
+    while (!Serial) {
+        ; //idle
+    }
+
+    Serial.println("Starting Test Sequence:");
+
+    Serial.println("Test-Block #0 - Setup")
+
+    Serial.println("Test-Block #0.1 - Setting Up...");
+    registerPattern = new byte[REGISTERS];
+
+    Serial.println("Test-Block #0.2 - Enabling Pins...");
+
     pinMode(DataPin, OUTPUT);
     pinMode(ClockPin, OUTPUT);
     pinMode(LatchPin, OUTPUT);
+    pinMode(OePin, OUTPUT);
+    pinMode(MrPin, OUTPUT);
+    pinMode(ledRPin, OUTPUT);
+    pinMode(ledGPin, OUTPUT);
+    pinMode(ledBPin, OUTPUT);
+    pinMode(switchOne, INPUT);
+    pinMode(switchTwo, INPUT);
+    pinmode(buzzerPin, OUTPUT);
+    pinmode(UsTrgPin, OUTPUT);
+    pinmode(UsEchPin, INPUT);
 
+    Serial.println("Pins enabled...");
 
-    //Create Array of bites for all registers
-    registerPattern = new byte[REGISTERS];
+    Serial.println("Test-Block #0 - Setup done")
 
-    clearRegisters();
+    Serial.println("Test-Block #1 - External Sensory")
+
+    Serial.println("Test-Block #1.1 - Testing LED...");
+    Serial.println("Red...")
+    digitalWrite(ledRPin, HIGH);
+    delay(1000);
+    digitalWrite(ledRPin, LOW);
+    delay(1000);
+    Serial.println("Green...")
+    digitalWrite(ledGPin, HIGH);
+    delay(1000);
+    digitalWrite(ledGPin, LOW);
+    delay(1000);
+    Serial.println("Blue...")
+    digitalWrite(ledBPin, HIGH);
+    delay(1000);
+    digitalWrite(ledBPin, LOW);
+    delay(500);
+
+    Serial.println("Test-Block #1.2 - Testing Ultrasonic Sensor...");
+    long duration;
+    int distance;
+
+    Serial.println("Taking Distance in 1 Second...")
+    digitalWrite(UsTrgPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(UsTrgPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(UsTrgPin, LOW);
+    duration = pulseIn(UsEchPin, HIGH);
+    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    Serial.println("Test-Block #1.3 - Testing Switches...");
+    int buttonTime = 2000;
+
+    Serial.println("Press Button 1...");
+    while (digitalRead(switchOne) != LOW)
+    {
+        ;
+    }
+    Serial.println("Button 1 pressed...");
+
+    Serial.println("Press Button 2...");
+    while (digitalRead(switchTwo) != LOW)
+    {
+        ;
+    }
+    Serial.println("Button 2 pressed...");
+    
+    Serial.println("Press both buttons for 2 sec...");
+    while (pulseIn(switchOne) <= buttonTime && pulseIn(switchTwo) <= buttonTime)
+    {
+        ;
+    }
+    Serial.println("Both buttons pressed...");
+
+    Serial.println("Test-Block #1.4 - Testing Buzzer...");
+
+    digitalWrite(buzzerPin, HIGH);
+    delay(1000);
+    digitalWrite(buzzerPin, LOW);
+
+    Serial.println("Test-Block #1 - External Sensory done");
+
+    Serial.println("Test-Block #2 - Internal Sensory");
+
+    Serial.println("Test-Block #2.1 - BME280 Test");
+    if (!bme.begin()) {
+        Serial.println("BME280 not found");
+        while (1);
+    }
+    Serial.println("Pressure:" + bme.readPressure());
+    Serial.println("Temperature:" + bme.readTemperature());    
+    Serial.println("Humidity:" + bme.readHumidity());
+    Serial.println("Altitude(calc):"+ bme.readAltitude(1013));
+
+    Serial.println("Test-Block #2.2 - DS1307 Test"); 
+    Wire.begin();
+    rtc.begin();
+    if (! rtc.isrunning()){
+        Serial.println("RTC Time not running...");
+        Serial.println("Setting to compiler time...");
+        rtc.adjust(DateTime(__DATE__,__TIME__));
+    }
+    DateTime now = rtc.now();
+    Serial.println("Time from RTC:");
+    Serial.print(now.day(), DEC);
+    Serial.print("/");
+    Serial.print(now.month(), DEC);
+    Serial.print("/");
+    Serial.print(now.year(), DEC);
+    Serial.print("");
+    Serial.print(now.hour(), DEC);
+    Serial.print(":");
+    Serial.print(now.minute(), DEC);
+    Serial.print(":");
+    Serial.print(now.second(), DEC); 
+    Serial.println();
+
+    Serial.println("Test-Block #2 - Internal Sensory done")
+
+    Serial.println("Test-Block #3 - Nixie-Tubes")
+
+    Serial.println("Check Digits...")
 
     checkDigits();
 
+    Serial.println("Hope all digits were in order :-)");
+
+    Serial.println("Test-Block #3 - Nixie-Tubes done")
 }
 
-//Give out a code for the user to see the failiure and print to serial
-void handleFail(long FaliureCode){
+void writeToShiftRegisters() {
+  digitalWrite(LatchPin, LOW);
 
-    Serial.println(FaliureCode);
+  //Go through stored patterns and write them in order
+  for (size_t i = 0; i < REGISTERS; i++) {
 
-    FaliureCode= FaliureCode*10 + 9;
-    convertToByte(FaliureCode);
+    byte * pattern = &registerPattern[i];
+
+    shiftOut(DataPin, ClockPin, MSBFIRST, *pattern);
+  }
+  digitalWrite(LatchPin, HIGH);
 }
 
-void clearRegisters(){
-    for (size_t i=0; i < REGISTERS; i++){
-        registerPattern[i] = 0;
+void writeStateToPin(int pin, bool state) {
+
+  //figure out shift reg nr (round down)
+  int shiftregnr = pin / 8;
+
+  //figure out pin on shift reg (remainder of div)
+  int pinnr = pin % 8;
+
+  //rewrite pattern
+  for (size_t i = 0; i < REGISTERS; i++) {
+    byte * pattern = &registerPattern[i];
+
+    if (i == shiftregnr) {
+      bitWrite(*pattern, pinnr, state);
     }
-}
-
-//figure out which pin on which register should be rewritten (state, bool)
-void writeStateToPin(int pin, bool state){
-  
-	//figure out shift reg nr (round down)
-  	int shiftregnr = pin / 8;
-  	
-  	//figure out pin on shift reg (remainder of div)
-  	int pinnr = pin % 8;
-  
-  	//rewrite pattern
-  	for(size_t i = 0; i < REGISTERS; i++){
-    	byte * pattern = &registerPattern[i];
-      	
-      	if (i == shiftregnr){
-          	bitWrite(*pattern, pinnr, state);
-        }
- 	}
+  }
 
 }
 
-//Write Pattern to Registers
-void writeToRegisters(){
-	digitalWrite(LatchPin, LOW);
-  	
-  	//Go through stored patterns and write them in order
-  	for(size_t i= 0; i < REGISTERS; i++){
-
-        byte * pattern = &registerPattern[i];
-
-      	shiftOut(DataPin, ClockPin, MSBFIRST, *pattern);
-    }
-  	digitalWrite(LatchPin, HIGH);
-}
-
-void checkDigits(){
-    int i = 0;
-    for(i = 0; i < REGISTERS; i++){
-        clearRegisters();
-        int x = 0;
-        for (x = 0; x < 10; x++){
-            byte y = byte(x);
-            registerPattern[i] = y;
-            writeToRegisters();
-            delay(100);
-        }
-        for (x = 0; x < 10; x++){
-            byte y = byte(x);
-            y = y << 4;
-            registerPattern[i] = y;
-            writeToRegisters();
-            delay(100);
-        }
-        delay(1000);
-    }
+void checkDigits() {
+  int i = 0;
+  for (i = 0; i < REGISTERS; i++) {
     clearRegisters();
-}
-
-//convert a seven digit number to bytecode, 6 Digits, 1 X
-void convertToByte(long number){
-
-    byte sendBytes[] = {0};
-    int firstDigits = number/100000;
-    int secondDigits = number/1000%100;
-    int thirdDigits = number/10 % 100;
-    int lastDigit = number % 10;
-
-    if (number < 1000000)
-    {
-        handleFail(900001);
+    int x = 0;
+    for (x = 0; x < 10; x++) {
+      byte y = byte(x);
+      registerPattern[i] = y;
+      writeToShiftRegisters();
+      delay(100);
     }
-    
-    else if (number <10000000)
-    {
-
-        int nr = firstDigits/10;
-        firstDigits = nr*6+firstDigits;
-
-        nr = secondDigits/10;
-        secondDigits = nr*6+secondDigits;
-
-        nr = thirdDigits/10;
-        thirdDigits = nr*6+thirdDigits;
-
-        byte bLastDigit = byte(lastDigit) << 4;
-
-        
-
-        sendBytes[3]= byte(firstDigits);
-        sendBytes[2]= byte(secondDigits);
-        sendBytes[1]= byte(thirdDigits);
-        sendBytes[0]= bLastDigit;
-
-        for(int i = 0; i < 4; i++){
-            registerPattern[i]= sendBytes[i];
-        }
-        writeToRegisters();
-
-
+    for (x = 0; x < 10; x++) {
+      byte y = byte(x);
+      y = y << 4;
+      registerPattern[i] = y;
+      writeToShiftRegisters();
+      delay(100);
     }
-    
-    else{ 
-        handleFail(900001);
-    }
-}
-
-//timer function, with int time in s
-void Timer(int time){
-
-}
-
-//alarm function
-void Alarm(long time){
-
+    delay(1000);
+  }
+  clearRegisters();
 }
 
 
