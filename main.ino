95294341f36c546d6f11a310ef39605a0455d2fb
bl44ck <bl4ckcr4nb3rry@gmail.com>
2022-03-13 22:14:35 +0100

    Added functionalty of a clock :X

--- main.ino
+++ main.ino
@@ -1,26 +1,27 @@
-// First we include the libraries
-//#include <Wire.h> activate for Arduino
+//Define Input-Pins
 
-//Pin connected to SH_CP of 74HC595 - pin 11 on ic
-int ClockPin = 2;
-//Pin connected to ST_CP of 74HC595 - pin 12 on ic
-int LatchPin = 3;
-//Pin connected to DS of 74HC595 - pin 14 on ic
-int DataPin = 4;
-//Pin connected to LED
-int ledRPin = 5;
-int ledGPin = 6;
-int ledBPin = 7;
-//Pin connected to Switch 1
-int switchOnePin = 11;
-//Pin connected to Switch 2
-int switchTwoPin = 12;
-//Pin connected to Buzzer
-int buzzerPin = 13;
+int ClockPin = 6;
+int LatchPin = 5;
+int DataPin = 2;
+int OePin = 3;
+int MrPin = 4;
+int ledRPin = 10;
+int ledGPin = 9;
+int ledBPin = 8;
+int switchOne = 15;
+int switchTwo = 14;
+int buzzerPin = 7;
+int UsTrgPin = 11;
+int UsEchPin = 12;
 
+//Define/Include Libraries
+#include <Wire.h>
+#include "RTClib.h"
+#include <Adafruit_BME280.h>
 
-int PlusPin = A1;
-int MinusPin = A2;
+//Define RTC and BME
+RTC_DS1307 rtc;
+Adafruit_BME280 bme;
 
 //Define Pins and Registers
 #define REGISTERS 4
@@ -29,211 +30,236 @@ int MinusPin = A2;
 //pointer
 byte * registerPattern;
 
-//define Alarmvar
-long AlarmTime = 0;
-
-
 void setup() {
-  
-    //Serial Output
-    Serial.begin(9600);
+  //Serial Output
+  Serial.begin(9600);
 
-    //set pins to output so you can control the shift register
-    pinMode(DataPin, OUTPUT);
-    pinMode(ClockPin, OUTPUT);
-    pinMode(LatchPin, OUTPUT);
-    pinMode(switchOne, INPUT);
-    pinMode(switchTwo, INPUT);
-    pinMode(buzzerPin, OUTPUT);
-    pinMode(ledRPin, OUTPUT);
-    pinMode(ledRPin, OUTPUT);
-    pinMode(ledRPin, OUTPUT);
+  Serial.println("Setup...");
+  //set pins to output so you can control the shift register
+  pinMode(DataPin, OUTPUT);
+  pinMode(ClockPin, OUTPUT);
+  pinMode(LatchPin, OUTPUT);
+  pinMode(OePin, OUTPUT);
+  pinMode(MrPin, OUTPUT);
+  pinMode(ledRPin, OUTPUT);
+  pinMode(ledGPin, OUTPUT);
+  pinMode(ledBPin, OUTPUT);
+  pinMode(switchOne, INPUT);
+  pinMode(switchTwo, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(UsTrgPin, OUTPUT);
+  pinMode(UsEchPin, INPUT);
+  //set SHR pins
+  digitalWrite(MrPin, HIGH);
+  digitalWrite(OePin, LOW);
 
-    //Create Array of bites for all registers
-    registerPattern = new byte[REGISTERS];
+  //Create Array of bites for all registers
+  registerPattern = new byte[REGISTERS];
 
-    clearRegisters();
+  //clear Clock
+  sendBlankToClock();
+  clearRegisters();
 
-    checkDigits();
+  //start clock
+  if (! bme.begin())
+  {
+    handleFail(300001);
+    delay(1000);
+  }
+  //start clock
+  Wire.begin();
+  rtc.begin();
+  if (! rtc.isrunning())
+  {
+    handleFail(300002);
+    delay(1000);
+  }
+  //Check digits for cathode poisoning
+  checkDigits();
 
 }
 
 //Give out a code for the user to see the failiure and print to serial
-void handleFail(long FaliureCode){
+void handleFail(long FaliureCode) {
 
-    Serial.println(FaliureCode);
-    notifyUser(1);
+  Serial.println(FaliureCode);
+  notifyUser(1);
 
-    FaliureCode= FaliureCode*10 + 9;
-    sendToClock(FaliureCode);
+  FaliureCode = FaliureCode * 10 + 7;
+  sendToClock(FaliureCode);
 }
 
-void clearRegisters(){
-    for (size_t i=0; i < REGISTERS; i++){
-        registerPattern[i] = 0;
-    }
+void clearRegisters() {
+  for (size_t i = 0; i < REGISTERS; i++) {
+    registerPattern[i] = 0b00000000;
+  }
 }
 
 //figure out which pin on which register should be rewritten (state, bool)
-void writeStateToPin(int pin, bool state){
-  
-	//figure out shift reg nr (round down)
-  	int shiftregnr = pin / 8;
-  	
-  	//figure out pin on shift reg (remainder of div)
-  	int pinnr = pin % 8;
-  
-  	//rewrite pattern
-  	for(size_t i = 0; i < REGISTERS; i++){
-    	byte * pattern = &registerPattern[i];
-      	
-      	if (i == shiftregnr){
-          	bitWrite(*pattern, pinnr, state);
-        }
- 	}
+void writeStateToPin(int pin, bool state) {
+
+  //figure out shift reg nr (round down)
+  int shiftregnr = pin / 8;
+
+  //figure out pin on shift reg (remainder of div)
+  int pinnr = pin % 8;
+
+  //rewrite pattern
+  for (size_t i = 0; i < REGISTERS; i++) {
+    byte * pattern = &registerPattern[i];
+
+    if (i == shiftregnr) {
+      bitWrite(*pattern, pinnr, state);
+    }
+  }
 
 }
 
 //Write Pattern to Registers
-void writeToRegisters(){
-	digitalWrite(LatchPin, LOW);
-  	
-  	//Go through stored patterns and write them in order
-  	for(size_t i= 0; i < REGISTERS; i++){
+void writeToShiftRegisters() {
+  digitalWrite(LatchPin, LOW);
 
-        byte * pattern = &registerPattern[i];
+  //Go through stored patterns and write them in order
+  for (size_t i = 0; i < REGISTERS; i++) {
 
-      	shiftOut(DataPin, ClockPin, MSBFIRST, *pattern);
-    }
-  	digitalWrite(LatchPin, HIGH);
+    if(i < 4) {
+      byte a = registerPattern[i];
+      byte b = a << 4;
+      byte c = a >> 4;
+      byte switchedPattern = b | c;
+      registerPattern[i] = switchedPattern;
+      }
+    byte * pattern = &registerPattern[i];
+
+    shiftOut(DataPin, ClockPin, MSBFIRST, *pattern);
+  }
+  digitalWrite(LatchPin, HIGH);
 }
 
-void checkDigits(){
-    int i = 0;
-    for(i = 0; i < REGISTERS; i++){
-        clearRegisters();
-        int x = 0;
-        for (x = 0; x < 10; x++){
-            byte y = byte(x);
-            registerPattern[i] = y;
-            writeToRegisters();
-            delay(100);
-        }
-        for (x = 0; x < 10; x++){
-            byte y = byte(x);
-            y = y << 4;
-            registerPattern[i] = y;
-            writeToRegisters();
-            delay(100);
-        }
-        delay(1000);
-    }
+//run through each digit as protection against cathode poisoning
+void checkDigits() {
+  int i = 0;
+  for (i = 0; i < REGISTERS; i++) {
     clearRegisters();
+    int x = 0;
+    for (x = 0; x < 10; x++) {
+      byte y = byte(x);
+      registerPattern[i] = y;
+      writeToShiftRegisters();
+      delay(200);
+    }
+    for (x = 0; x < 10; x++) {
+      byte y = byte(x);
+      y = y << 4;
+      registerPattern[i] = y;
+      writeToShiftRegisters();
+      delay(200);
+    }
+    delay(200);
+  }
+  clearRegisters();
 }
 
 //convert a seven digit number to bytecode, 6 Digits, 1 X
-void sendToClock(long number){
+void sendToClock(long number) {
 
-    byte sendBytes[] = {0};
-    int firstDigits = number/100000;
-    int secondDigits = number/1000%100;
-    int thirdDigits = number/10 % 100;
-    int lastDigit = number % 10;
+  byte sendBytes[] = {0b00000000, 0b00000000, 0b00000000, 0b00000000};
+  int firstDigits = number / 100000;
+  int secondDigits = number / 1000 % 100;
+  int thirdDigits = number / 10 % 100;
+  int lastDigit = number % 10;
 
-    if (number < 1000000)
+  int nr = firstDigits / 10;
+  firstDigits = nr * 6 + firstDigits;
+
+  nr = 0;
+
+  nr = secondDigits / 10;
+  secondDigits = nr * 6 + secondDigits;
+
+  nr = 0;
+
+  nr = thirdDigits / 10;
+  thirdDigits = nr * 6 + thirdDigits;
+
+  byte bLastDigit = byte(lastDigit) << 4;
+  bLastDigit = bLastDigit | 0b00001111; //add 1111 to activate transistors
+
+  sendBytes[3] = byte(firstDigits);
+  sendBytes[2] = byte(secondDigits);
+  sendBytes[1] = byte(thirdDigits);
+  sendBytes[0] = bLastDigit;
+
+  for (int i = 0; i < 4; i++) {
+    registerPattern[i] = sendBytes[i];
+  }
+  writeToShiftRegisters();
+
+
+}
+
+void sendBlankToClock() {
+  //blank the clock
+  byte sendBytes[] {0b11111111, 0b11111111, 0b11111111, 0b11111111};
+
+  for (int i = 0; i < 4; i++) {
+    registerPattern[i] = sendBytes[i];
+  }
+
+  writeToShiftRegisters();
+}
+
+
+
+
+void notifyUser(int type) {
+  //types 0: info (short) 1: warn (long) 2: error (until solved) 3: alarm (until reset/button)
+  if (type = 0) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(500);
+    digitalWrite(buzzerPin, LOW);
+  }
+  else if (type = 1) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(2000);
+    digitalWrite(buzzerPin, LOW);
+  }
+  else if (type = 2)
+  {
+    /* code */
+  }
+  else if (type = 3)
+  {
+    for (size_t i = 0; i < 10000; i++)
     {
-        handleFail(900001);
+      digitalWrite(buzzerPin, HIGH);
+      delay(1000);
+      digitalWrite(buzzerPin, LOW);
+      delay(1000);
     }
-    
-    else if (number <10000000)
-    {
 
-        int nr = firstDigits/10;
-        firstDigits = nr*6+firstDigits;
+  }
 
-        nr = secondDigits/10;
-        secondDigits = nr*6+secondDigits;
-
-        nr = thirdDigits/10;
-        thirdDigits = nr*6+thirdDigits;
-
-        byte bLastDigit = byte(lastDigit) << 4;
-
-        
-
-        sendBytes[3]= byte(firstDigits);
-        sendBytes[2]= byte(secondDigits);
-        sendBytes[1]= byte(thirdDigits);
-        sendBytes[0]= bLastDigit;
-
-        for(int i = 0; i < 4; i++){
-            registerPattern[i]= sendBytes[i];
-        }
-        writeToRegisters();
-
-
-    }
-    
-    else{ 
-        handleFail(900002);
-    }
-}
-
-//timer function, with int time in s
-void Timer(int time){
 
 }
 
-//alarm functions
-
-void getAlarm(){
-    
+//function to Display Time
+void displayTime() {
+  DateTime now = rtc.now();
+  int hourNow = now.hour();
+  int minuteNow = now.minute();
+  int secondNow = now.second();
+  long timeNow = 0;
+  timeNow = (hourNow * 100000) + (minuteNow * 1000) + (secondNow * 10);
+  sendToClock(timeNow);
 }
-
-void setAlarm(long time){
-
-}
-
-//notify user
-
-void notifyUser(int type){
-    //types 0: info (short) 1: warn (long) 2: error (until solved) 3: alarm (until reset/button)
-    if(type=0){
-        digitalWrite(buzzerPin, HIGH);
-        delay(500);
-        digitalWrite(buzzerPin, LOW);
-    }
-    else if(type=1){
-        digitalWrite(buzzerPin, HIGH);
-        delay(5000);
-        digitalWrite(buzzerPin, LOW);
-    }
-    else if (type = 2)
-    {
-        /* code */
-    }
-    else if (type = 3)
-    {
-        for (size_t i = 0; i < 10000; i++)
-        {
-        digitalWrite(buzzerPin, HIGH);
-        delay(1000);
-        digitalWrite(buzzerPin, LOW);
-        delay(1000);
-        }
-        
-    }
-    
-    
-}
-
 
 //MAIN LOOP
 void loop() {
+  
+  displayTime();
 
-    sendToClock(4544122);
-
-    delay(10000);
+  delay(100);
 }
 
 // 0000 0000 0000 0000 0000 0000 0000
@@ -259,4 +285,3 @@ void loop() {
 // 112-121= 70 - 79
 // 128-137= 80 - 89
 // 144-153= 90 - 99
-
